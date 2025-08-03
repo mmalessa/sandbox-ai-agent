584f54871f889b9099b81e2baa0f93892bf0b402
Marcin Malessa <marcin@malessa.pl>
2025-08-03 16:24:48 +0200

    Add simple chat

--- go-client/go.mod
+++ go-client/go.mod
@@ -3,6 +3,7 @@ module go-client
 go 1.24.5
 
 require (
+	github.com/gorilla/websocket v1.5.3
 	github.com/sashabaranov/go-openai v1.40.5
 	gopkg.in/yaml.v3 v3.0.1
 )
--- go-client/main.go
+++ go-client/main.go
@@ -4,19 +4,77 @@ import (
 	"context"
 	"fmt"
 	"log"
+	"net/http"
 	"os"
+
+	"github.com/gorilla/websocket"
 
 	openai "github.com/sashabaranov/go-openai"
 	"gopkg.in/yaml.v3"
 )
 
+var client *openai.Client
+var cfg *chatConfig
+var messages []openai.ChatCompletionMessage
+
 type chatConfig struct {
 	Model       string  `yaml:"model"`
 	Temperature float32 `yaml:"temperature"`
-	Messages    []struct {
-		Role    string `yaml:"role"`
-		Content string `yaml:"content"`
-	} `yaml:"messages"`
+	System      string  `yaml:"system"`
+}
+
+// upgrader changes HTTP connection to WebSocket
+var upgrader = websocket.Upgrader{
+	CheckOrigin: func(r *http.Request) bool {
+		return true // we allow to connect from any source
+	},
+}
+
+func wsHandler(w http.ResponseWriter, r *http.Request) {
+	// upgrade connection HTTP to WebSocket
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Println("Upgrade error:", err)
+		return
+	}
+	defer conn.Close()
+
+	log.Println("New WebSocket connection")
+
+	for {
+		_, msg, err := conn.ReadMessage()
+		if err != nil {
+			log.Println("Read error:", err)
+			break
+		}
+
+		log.Printf("Received: %s", msg)
+		messages = append(messages, openai.ChatCompletionMessage{Role: "user", Content: string(msg)})
+
+		// response logic
+		log.Println("Sending to AI")
+		resp, err := client.CreateChatCompletion(
+			context.Background(),
+			openai.ChatCompletionRequest{
+				Model:       cfg.Model,
+				Temperature: cfg.Temperature,
+				Messages:    messages,
+			},
+		)
+		if err != nil {
+			log.Fatalf("Sending request ERROR: %v", err)
+		}
+		response := resp.Choices[0].Message.Content
+		log.Println(response)
+
+		messages = append(messages, openai.ChatCompletionMessage{Role: "assistant", Content: response})
+
+		// send response to web client
+		if err := conn.WriteMessage(websocket.TextMessage, []byte(response)); err != nil {
+			log.Println("Write error:", err)
+			break
+		}
+	}
 }
 
 func loadChatConfig(path string) (*chatConfig, error) {
@@ -33,20 +91,8 @@ func loadChatConfig(path string) (*chatConfig, error) {
 	return &cfg, nil
 }
 
-func getChatCompletionMessages(chatConfig *chatConfig) []openai.ChatCompletionMessage {
-	messages := make([]openai.ChatCompletionMessage, len(chatConfig.Messages))
-	for i, m := range chatConfig.Messages {
-		messages[i] = openai.ChatCompletionMessage{
-			Role:    m.Role,
-			Content: m.Content,
-		}
-	}
-
-	return messages
-}
-
-func main() {
-
+func initAiClient() {
+	// ai client
 	baseURL := os.Getenv("OPENAI_URL")
 	if baseURL == "" {
 		baseURL = "http://localhost:11434/v1"
@@ -57,30 +103,27 @@ func main() {
 		apiToken = "DefaultToken"
 	}
 
-	cfg, err := loadChatConfig("chat.yaml")
+	var err error
+	cfg, err = loadChatConfig("chat.yaml")
 	if err != nil {
 		log.Fatal("error loading YAML:", err)
 	}
 
 	config := openai.DefaultConfig(apiToken)
 	config.BaseURL = baseURL
-	client := openai.NewClientWithConfig(config)
+	client = openai.NewClientWithConfig(config)
 
-	messages := getChatCompletionMessages(cfg)
+	messages = append(messages, openai.ChatCompletionMessage{Role: "system", Content: cfg.System})
+}
 
-	fmt.Println("Sending request")
-	resp, err := client.CreateChatCompletion(
-		context.Background(),
-		openai.ChatCompletionRequest{
-			Model:       cfg.Model,
-			Temperature: cfg.Temperature,
-			Messages:    messages,
-		},
-	)
-	if err != nil {
-		log.Fatalf("Sending request ERROR: %v", err)
-	}
+func main() {
 
-	fmt.Println(resp.Choices[0].Message.Content)
-	fmt.Println("\n---------------------")
+	initAiClient()
+
+	// http/websocket server
+	http.Handle("/", http.FileServer(http.Dir("./static")))
+	http.HandleFunc("/ws", wsHandler)
+
+	log.Println("Serwer startuje na :8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
